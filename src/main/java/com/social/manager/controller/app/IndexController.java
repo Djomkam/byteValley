11a5088e86752384a6c770aadfe9f3151987af5e
yongphelan <yongn.phelan@gmail.com>
2015-11-15 13:43:40 -0500

    using ajax to get json data in spring

--- src/main/java/com/social/manager/controller/app/IndexController.java
+++ src/main/java/com/social/manager/controller/app/IndexController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.JsonObject;
 
 @Controller
 public class IndexController {
@@ -12,5 +16,20 @@ public class IndexController {
 	public String homepage(Model model)
 	{
 		return "index";
+	}
+	
+	
+	@RequestMapping(value="/test", method=RequestMethod.POST, produces="application/json")
+	@ResponseBody
+	public String test(@RequestParam("id") int id) {
+		System.out.println(id + " id");
+		
+		
+		JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("id", id);
+        
+        System.out.println(jsonObject);
+        
+		return jsonObject.toString();
 	}
 }
