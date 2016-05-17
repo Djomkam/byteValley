ee570dde289283cd7f35aad09bc54d3392d4dbd0
Djomkam <djomkamk@gmail.com>
2016-05-17 23:00:19 +0530

    yesoooh
    yesoooooh

--- src/main/java/com/social/manager/controller/app/IndexController.java
+++ src/main/java/com/social/manager/controller/app/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.JsonObject;
 
@@ -13,9 +14,11 @@ import com.google.gson.JsonObject;
 public class IndexController {
 
 	@RequestMapping(value="/index",method = RequestMethod.GET)
-	public String homepage(Model model)
+	public ModelAndView homepage(Model model)
 	{
-		return "index";
+		ModelAndView view = new ModelAndView();
+		view.setViewName("index");
+		return view;
 	}
 	
 	
